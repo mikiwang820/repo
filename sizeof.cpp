038991bf4ad1d58971abbdd0158f155535e416e0
mikiwang820 <1104105210@nkust.edu.tw>
2021-09-16 10:23:25 +0800

    sizeof practice

---
+++ sizeof.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+double dType;
+cout<<"the data size  of int is "<<sizeof(int)<<" bytes\n";
+cout<<"the data size of dType is "<<sizeof(dType)<<" bytes\n";
+//system("PAUSE");
+return EXIT_SUCCESS;
+}
